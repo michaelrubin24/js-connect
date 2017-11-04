c0219780d4462f7c7ac872878b645db11fd716ec
Michael Rubinshtein <deliverypoint7@yandex.ru>
2017-11-04 21:13:51 +0300

    edited add of last element

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@
 var cards = ["Валет", "Дама", "Джокер", "Туз", "Король"];
 var i = 0;
 var cards_str = " ";
-card_last = cards.length - 1;
+var card_last = cards.length - 1;
 while (i < card_last){
   cards_str += cards[i] + ", ";
   i++;
@@ -16,8 +16,7 @@ var card_new;
 while (card_new != "стоп"){
   card_new = prompt("Что добавить в конец массива?").toLowerCase();
   if (card_new != "стоп" && card_new != ""){
-    cards += ", " + card_new;
-    console.log(cards);
+    cards[cards.length] = card_new;
     alert(cards);
   }
 }
